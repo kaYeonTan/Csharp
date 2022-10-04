9a2ac00e4ea02633cbd1b43fd28b7c26001b8e09
SUWAN <106606698+kaYeonTan@users.noreply.github.com>
2022-10-04 09:06:05 +0900

    1004

---
+++ 1004/1004/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //화면에 문자를 출력한다. 
+            Console.WriteLine("어서 오세요 C#으로!");
+            Console.WriteLine("C#을 시작합시다!");
+        }
+    }
+}
