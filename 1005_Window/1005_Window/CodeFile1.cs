f5b83a961f1b046f4c696dc89ce659f4859c3c76
SUWAN <106606698+kaYeonTan@users.noreply.github.com>
2022-10-05 19:11:10 +0900

    1005cs

---
+++ 1005_Window/1005_Window/CodeFile1.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+class Sample2
+{
+    public static void Main()
+    {
+        Form fm; //윈도(폼)에 붙이는 이름을 준비한다.  
+        fm = new Form();
+
+        fm.Text = "어서오세요 C#으로!";
+
+        Application.Run(fm);
+    }
+}
