71840b23c52354d9f6723448d64d457448671e87
SUWAN <106606698+kaYeonTan@users.noreply.github.com>
2022-10-06 19:08:48 +0900

    1006Practice

---
+++ 1006_2.4/1006_2.4/Practice.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+class Practice
+{
+    public static void Main()
+    {
+        Form fm; //윈도(폼)에 붙이는 이름을 준비한다.  
+        fm = new Form();
+
+        fm.Text = "샘플!";
+
+        Label lb = new Label();
+        lb.Width = 300;
+        lb.Text = "또 만납시다!";
+        lb.Parent = fm;
+
+        Application.Run(fm);
+    }
+}
