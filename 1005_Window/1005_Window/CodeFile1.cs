a8017b3133da2dc91714f35a164c537eb0d9003e
SUWAN <106606698+kaYeonTan@users.noreply.github.com>
2022-10-12 09:20:17 +0900

    cs1012

--- 1005_Window/1005_Window/CodeFile1.cs
+++ 1005_Window/1005_Window/CodeFile1.cs
@@ -16,8 +16,14 @@ class Sample2
         //lb.Text = "C#을 시작합시다 (❁´◡`❁)"; //라벨을 폼에 올린다.
         //lb.Parent = fm;               //라벨의 타이틀을 실행한다.
 
+        int w;  //변수를 선언한다.
+        w = 100;//변수에 값을 대입한다.
+
         PictureBox pb = new PictureBox(); //이미지를 읽어 들이는 픽쳐박스를 작성한다. 
         pb.Image = Image.FromFile("C:\\Users\\수완\\dog.bmp"); //이미지를 읽어들인다.
+        pb.Top = (fm.ClientSize.Height - pb.Height) / 2;
+        pb.Left = (fm.ClientSize.Width - pb.Width) / 2;
+
         pb.Parent = fm;
 
         Application.Run(fm);
---
+++ Practice1012 3.4/Practice1012 3.4/CodeFile1.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+class Sample2
+{
+    public static void Main()
+    {
+        Form fm = new Form();
+
+        fm.Text = "샘플";
+        fm.Width = 300;
+        fm.Height = 200;
+
+        Label lb = new Label();
+        lb.Text = "안녕하세요";
+        lb.Top = (fm.ClientSize.Height - lb.Height) / 2;
+        lb.Left = (fm.ClientSize.Width - lb.Width) / 2;
+        lb.Parent = fm;
+
+        Application.Run(fm);
+    }
+}
