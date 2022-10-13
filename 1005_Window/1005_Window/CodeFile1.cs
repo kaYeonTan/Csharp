feee53e453c1153c0d0a5613be78aa85a323dfaa
SUWAN <106606698+kaYeonTan@users.noreply.github.com>
2022-10-13 09:53:27 +0900

    if~else1013

--- 1005_Window/1005_Window/CodeFile1.cs
+++ 1005_Window/1005_Window/CodeFile1.cs
@@ -5,25 +5,26 @@ class Sample2
 {
     public static void Main()
     {
-        Form fm; //윈도(폼)에 붙이는 이름을 준비한다.  
+        Form fm; 
         fm = new Form();
 
-        fm.Text = "어서오세요 C#으로!";
+        fm.Text = "샘플";
+        fm.Width = 300; fm.Height = 200;
 
-        //Label lb = new Label(); //라벨을 작성한다.
-        //lb.Width = 300;         //라벨의 가로 크기를 설정한다.
+        PictureBox pb = new PictureBox();
+        pb.Image = Image.FromFile("C:\\Users\\수완\\dog.bmp");
+        pb.Left = 0;
 
-        //lb.Text = "C#을 시작합시다 (❁´◡`❁)"; //라벨을 폼에 올린다.
-        //lb.Parent = fm;               //라벨의 타이틀을 실행한다.
+        Label lb = new Label();
+        lb.Top = pb.Bottom;
+        lb.Width = 170;
+        lb.Text = "강아지 입니다";
 
-        int w;  //변수를 선언한다.
-        w = 100;//변수에 값을 대입한다.
+        if (pb.Left >= 150)     lb.Text = "강아지는 동쪽에 있습니다.";
+        else if (pb.Left <= 20) lb.Text = "강아지는 서쪽에 있습니다.";
+        else                    lb.Text = "강아지는 중앙에 있습니다.";
 
-        PictureBox pb = new PictureBox(); //이미지를 읽어 들이는 픽쳐박스를 작성한다. 
-        pb.Image = Image.FromFile("C:\\Users\\수완\\dog.bmp"); //이미지를 읽어들인다.
-        pb.Top = (fm.ClientSize.Height - pb.Height) / 2;
-        pb.Left = (fm.ClientSize.Width - pb.Width) / 2;
-
+        lb.Parent = fm;
         pb.Parent = fm;
 
         Application.Run(fm);
