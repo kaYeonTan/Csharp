ebbc8a048d0c8a50992fbe58fdc4ac7a925e914c
SUWAN <106606698+kaYeonTan@users.noreply.github.com>
2022-10-18 22:31:36 +0900

    cs_switch

--- 1005_Window/1005_Window/CodeFile1.cs
+++ 1005_Window/1005_Window/CodeFile1.cs
@@ -13,16 +13,17 @@ class Sample2
 
         PictureBox pb = new PictureBox();
         pb.Image = Image.FromFile("C:\\Users\\수완\\dog.bmp");
-        pb.Left = 0;
+        pb.Left = 400;
 
         Label lb = new Label();
         lb.Top = pb.Bottom;
         lb.Width = 170;
         lb.Text = "강아지 입니다";
 
-        if (pb.Left >= 150)     lb.Text = "강아지는 동쪽에 있습니다.";
-        else if (pb.Left <= 20) lb.Text = "강아지는 서쪽에 있습니다.";
-        else                    lb.Text = "강아지는 중앙에 있습니다.";
+        if (pb.Left >= 0 && pb.Left <= fm.Width)
+            lb.Text = "강아지는 화면 안에 있습니다.";
+        else
+            lb.Text = "강아지는 화면 밖에 있습니다.";
 
         lb.Parent = fm;
         pb.Parent = fm;
