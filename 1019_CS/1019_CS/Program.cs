8dfd7b902fcbf403041cc8205aadf806dfc93bcd
SUWAN <106606698+kaYeonTan@users.noreply.github.com>
2022-10-19 19:23:15 +0900

    1019_forWhile

---
+++ 1019_CS/1019_CS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+class Sample5
+{
+    public static void Main()
+    {
+        Form fm;
+        fm = new Form();
+
+        fm.Text = "샘플";
+        fm.Width = 300; fm.Height = 150;
+
+        Label lb = new Label();
+        lb.Width = fm.Width; lb.Height = fm.Height;
+
+        for (int i = 0; i < 5; i++)
+        {
+            lb.Text += i + "번의 강아지를 표시합니다.\n";
+        };
+
+        lb.Parent = fm;
+
+        Application.Run(fm);
+    }
+}
