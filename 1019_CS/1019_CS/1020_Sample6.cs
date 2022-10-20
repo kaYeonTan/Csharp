7b85b545eed1ad71673d1cdb74fbcd340276cd80
SUWAN <106606698+kaYeonTan@users.noreply.github.com>
2022-10-20 20:36:44 +0900

    cs1020

---
+++ 1019_CS/1019_CS/1020_Sample6.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+class Sample6
+{
+    public static void Sample()
+    {
+        Form fm;
+        fm = new Form();
+
+        fm.Text = "샘플";
+
+        PictureBox[] pb = new PictureBox[5];
+
+        Label lb = new Label();
+        lb.Width = fm.Width; lb.Height = fm.Height;
+
+        for (int i = 0; i < pb.Length; i++)
+        {
+            pb[i] = new PictureBox();
+            pb[i].Image = Image.FromFile("C:\\Users\\수완\\dog.bmp");
+            pb[i].Top = i * pb[i].Height;
+            pb[i].Parent = fm;
+        }
+
+        Application.Run(fm);
+    }
+}
---
+++ 1019_CS/1019_CS/1020_Sample7.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+class Sample7
+{
+    public static void Main()
+    {
+        Form fm = new Form();
+        fm.Text = "샘플";
+        fm.Width = 250; fm.Height = 100;
+
+        Label lb = new Label();
+        lb.Width = fm.Width; lb.Height = fm.Height;
+
+        string[] str = new string[3] { "연필", "지우기", "자" };
+
+        foreach(var s in str) //foreach문으로 모든 배열 요소를 돌면서 꺼낸다
+        {
+            lb.Text += s + "\n";
+        };
+
+        lb.Parent = fm;
+
+        Application.Run(fm);
+    }
+}
