78f2e738b4f4958dca26421e3e29bdcb2199651b
SUWAN <106606698+kaYeonTan@users.noreply.github.com>
2022-10-25 08:39:01 +0900

    test4.5_1025

---
+++ 4.5_test1025/4.5_test1025/Test1.cs
@@ -0,0 +1,33 @@
+﻿/*using System.Windows.Forms;
+using System.Drawing;
+
+public class Test1
+{
+    public static void Main()
+    {
+        Form fm = new Form();
+        fm.Text = "샘플";
+        fm.Width = 250; fm.Height = 150;
+
+        Label lb = new Label();
+        lb.Width = fm.Width; lb.Height = fm.Height;
+
+        //for (int i = 1; i < 11; i++)
+        //{
+        //    if (i % 2 == 0) 
+        //        lb.Text += i + "을(를) 표시합니다.\n";
+        //}
+
+        int a = 2;
+        while (a < 11)
+        {
+            lb.Text += a + "을(를) 표시합니다.\n";
+            a += 2;
+        };
+
+        lb.Parent = fm;
+
+        Application.Run(fm);
+    }
+}
+*/
---
+++ 4.5_test1025/4.5_test1025/Test2.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+public class Test2
+{
+    public static void Main()
+    {
+        Form fm = new Form();
+        fm.Text = "샘플";
+        fm.Width = 600; fm.Height = 300;
+
+        PictureBox[,] dog = new PictureBox[5,5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                dog[i,j] = new PictureBox();
+                dog[i, j].Image = Image.FromFile("C:\\Users\\수완\\dog.bmp");
+                dog[i, j].Left = dog[i, j].Width * i;
+                dog[i, j].Top = j * dog[i, j].Height;
+                dog[i, j].Parent = fm;
+            }
+        }
+
+        Application.Run(fm);
+    }
+}
