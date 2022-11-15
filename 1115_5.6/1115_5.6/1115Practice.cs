c46c5a17619eac284db91a10592caa0542ebf5cf
SUWAN <106606698+kaYeonTan@users.noreply.github.com>
2022-11-15 10:10:01 +0900

    5.6

---
+++ 1115_5.6/1115_5.6/1115Practice.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+class Sample1
+{
+    public static void Main()
+    {
+        Form fm = new Form();
+        fm.Text = "샘플";
+        fm.Width = 250; fm.Height = 100;
+
+        Label lb = new Label();
+
+        Ball bl = new Ball();
+        bl.Move();
+
+        lb.Text = "공의 위치는 \nTop:" + bl.Top + "Left:" + bl.Left + "입니다.";
+        lb.Parent = fm;
+
+        Application.Run(fm);
+    }
+
+}
+class Ball
+{
+    int top;
+    int left;
+
+    public Ball()
+    {
+        top = 0;
+        left = 0;
+    }
+    public void Move()
+    {
+        top = top + 10;
+        left = left + 10;
+    }
+    public int Top
+    {
+        set { top = value; }
+        get { return top; }
+    }
+    public int Left
+    {
+        set { left = value; }
+        get { return left; }
+    }
+}
---
+++ 1115_5.6/1115_5.6/1115Practice2.cs
@@ -0,0 +1,35 @@
+﻿/*using System.Windows.Forms;
+using System.Drawing;
+
+class Sample2 : Form
+{ 
+    public static void Main()
+    {
+        Application.Run(new Sample2());
+    }
+    public Sample2()
+    {
+        this.Text = "샘플";
+        this.Width = 400; this.Height = 200;
+
+        whiteLabel wl1 = new whiteLabel();
+        wl1.Text = "안녕하세요";
+
+        whiteLabel wl2 = new whiteLabel();
+        wl2.Text = "감사합니다.";
+
+        wl2.Left = wl2.Left + 150;
+
+        wl1.Parent = this;
+        wl2.Parent = this;
+    }       
+}
+
+class whiteLabel : Label 
+{
+    public whiteLabel()
+    {
+        this.BackColor = Color.White;
+    }
+}
+*/
