90fed61d7bd0d01e5753db1b058683159746b359
SUWAN <106606698+kaYeonTan@users.noreply.github.com>
2022-12-07 10:53:57 +0900

    cs

---
+++ 1207_CS6.1/1207_CS6.1/CodeFile1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Sample1 : Form
+{
+    Label lb;
+
+    public static void Main()
+    {
+        Application.Run(new Sample1());
+    }
+
+    public Sample1()
+    {
+        this.Text = "샘플";
+        this.Width = 250; this.Height = 200;
+        lb = new Label();
+        lb.Text = "어서 오세요";
+        lb.Parent = this;
+        this.Click += new EventHandler(fm_Click);
+    }
+    public void fm_Click(object sender, EventArgs e)
+    {
+        lb.Text = "안녕하세요";    
+    }
+}
---
+++ 1207_CS6.1/1207_CS6.1/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _1207_CS6._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
